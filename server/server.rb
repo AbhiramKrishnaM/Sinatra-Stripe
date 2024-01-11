277d6b63d2647961ea7fdfed8019969929b5f3ad
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2024-01-11 23:27:02 +0530

    added file join and html resonse

--- server/server.rb
+++ server/server.rb
@@ -5,12 +5,15 @@ require "sinatra"
 Dotenv.load # for loading environment variables
 Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 
-puts Stripe::Plan.list
+
 
 set :static, true
-set :public_folder, "../client"
+set :public_folder, File.join(File.dirname(__FILE__), ENV["STATIC_DIR"])
 set :port, 3000
 
+
+
 get '/' do
-  return "Hello how are you ?"
+  content_type "text/html"
+  send_file File.join(settings.public_folder, "index.html")
 end
